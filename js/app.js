108c03f959d19b7cf9fc5b2c8430054c78a27be8
AraceliGS <aragutarras@gmail.com>
2018-02-27 10:36:51 -0500

    añadiendo avances en la funcionalidad- reutilizando el componente Cabecera

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 const div = React.DOM.div;
 const h2 = React.DOM.h2;
 
-function Cabecera() {
-  const titulo = 'Hola Mundo';
+function Cabecera(props) {
+  const titulo = 'Hola ' + props.saludo;
   const elemento = div(null, h2(null, titulo));
 
   return elemento;
 }
 
-ReactDOM.render(Cabecera(), document.getElementById('react-container'));
+ReactDOM.render(
+  Cabecera({saludo: 'Mundo'}), document.getElementById('cabecera-1')
+);
+
+ReactDOM.render(
+  Cabecera({saludo: 'Amigx'}),
+  document.getElementById('cabecera-2')
+);
