aba62842d4058198fd5c7e775795cd7c21ec8cd3
AraceliGS <aragutarras@gmail.com>
2018-02-27 10:07:02 -0500

    añadiendo funcionalidad con react- mostrando en consola Hola Mundo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+const div = React.DOM.div;
+const h2 = React.DOM.h2;
 
+function Cabecera() {
+  const titulo = 'Hola Mundo';
+  const elemento = div(null, h2(null, titulo));
+
+  return elemento;
+}
+
+ReactDOM.render(Cabecera(), document.getElementById('react-container'));
