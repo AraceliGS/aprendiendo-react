4c4609374fe1d4157b0090cf34a8a5d459f74c0d
AraceliGS <aragutarras@gmail.com>
2018-02-28 10:06:11 -0500

    añadiendo avances en la clase

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
 // document.getElementById('box');
 
-const title = React.createElement('h1', {id: 'title'}, 'Hola Mundo');
+// const title = React.createElement('h1', {id: 'title'}, 'Hola Mundo');
+const title = (
+  <div>
+    <h1 id="title">Hola
+      <span> Mundo</span>
+    </h1>
+    <h2>Sleep</h2>
+  </div>
+);
 
 ReactDOM.render(title, document.getElementById('box'));
