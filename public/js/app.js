03c21a8d71f01795b9e6e881f325d5fcced4e6f1
AraceliGS <aragutarras@gmail.com>
2018-02-28 18:28:23 -0500

    añadiendo pagina cuyos elementos son creado con REACT segùn el JSX

--- public/js/app.js
+++ public/js/app.js
@@ -10,15 +10,40 @@ const mainTitle = (
 
 ReactDOM.render(mainTitle, document.getElementById('header'));
 
-const articleSection = {
-  <div>
-  <div>
-} 
-
-/*const mainTitle = (
-  <div className={'color'}>
-    <h1>Getting Started</h1>
-    <p>Descripción</p>
+const articleSection = (
+  <div className={'article-section'}>
+    <h2>Main Section</h2>  
+    <article>
+      <h3>Título 1</h3>
+      <p>Descripción 1</p>
+      <hr/>
+    </article>
+    <article>
+      <h3>Título 2</h3>
+      <p>Descripción 2</p>
+      <hr/>
+    </article>
+    <article>
+      <h3>Título 3</h3>
+      <p>Descripción 3</p>
+      <hr/>
+    </article>
   </div>
 );
-*/
+
+ReactDOM.render(articleSection, document.getElementById('main'));
+
+const linksSection = (
+  <div className={'links-section'}>
+    <h4>Links</h4>
+    <ul>
+      <li><a href='#'>Link 1</a></li>
+      <li><a href='#'>Link 2</a></li>
+      <li><a href='#'>Link 3</a></li>
+      <li><a href='#'>Link 4</a></li>
+      <li><a href='#'>Link 5</a></li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(linksSection, document.getElementById('aside'));
